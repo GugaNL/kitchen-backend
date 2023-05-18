756bd3263a517c129c464ab7b6dee3a16010df24
Gustavo Lucena <gustavo.lucena@compasso.com.br>
2023-05-18 17:40:29 -0300

    change location knex config

--- src/index.js
+++ src/index.js
@@ -2,15 +2,7 @@ import { ApolloServer } from 'apollo-server';
 import { typeDefs, resolvers } from './graphql/schema';
 import { CategorySQLDataSource } from './graphql/schema/category/datasources';
 import { RecipeSQLDataSource } from './graphql/schema/recipe/datasources';
-
-const knexConfig = {
-  client: process.env.DATABASE_CLIENT,
-  connection: {
-    database: process.env.DATABASE_NAME,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-  },
-};
+import { knexConfig } from './knex';
 
 const dbCategory = new CategorySQLDataSource(knexConfig);
 const dbRecipes = new RecipeSQLDataSource(knexConfig);
--- src/knex/index.js
+++ src/knex/index.js
@@ -2,3 +2,13 @@ const knexFn = require('knex');
 const knexfile = require('./knexfile');
 
 export const knex = knexFn(knexfile[process.env.NODE_ENV]);
+
+
+export const knexConfig = {
+  client: process.env.DATABASE_CLIENT,
+  connection: {
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+  },
+};
