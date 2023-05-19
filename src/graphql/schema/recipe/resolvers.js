c3858378910b6c03a9405f736883b32b26258545
Gustavo Lucena <gustavo.lucena@compasso.com.br>
2023-05-19 11:02:08 -0300

    category dataloader

--- src/graphql/schema/recipe/resolvers.js
+++ src/graphql/schema/recipe/resolvers.js
@@ -1,3 +1,24 @@
+const DataLoader = require('dataloader');
+
+//Category field
+// const category = async ({ id: category_id }, _, { dataSources }) => {
+//   const response = await dataSources.dbCategory.getCategory(category_id);
+//   return response;
+// };
+
+const category = async ({ id: category_id }, _, { dataSources }) => {
+  const categoryLoader = new DataLoader(async (keys) => {
+    const result = keys.map(async (categoryId) => {
+      const response = await dataSources.dbCategory.getCategory(categoryId);
+      return response;
+    });
+
+    return Promise.resolve(result);
+  });
+
+  return categoryLoader.load(category_id);
+};
+
 const recipes = async (_, __, { dataSources }) => {
   const response = dataSources.dbRecipe.listRecipes();
   return response;
@@ -6,9 +27,10 @@ const recipes = async (_, __, { dataSources }) => {
 const recipe = async (_, { id }, { dataSources }) => {
   const response = await dataSources.dbRecipe.getRecipe(id);
   return response;
-}
+};
 
 export const recipeResolvers = {
-  Query: { recipe, recipes },
+  Query: { recipe, recipes, category },
+  Recipe: { category },
   //Mutation: { createCategory, updateCategory, deleteCategory },
 };
